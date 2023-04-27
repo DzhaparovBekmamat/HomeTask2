e6e80e67d7d9455f256166d14783389510633f4a
DzhaparovBekmamat <dzhaparovbekmamat03@gmail.com>
2023-04-27 19:47:52 +0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/hometask2/AddFragment.kt

--- app/src/main/java/com/example/hometask2/AddFragment.kt
+++ app/src/main/java/com/example/hometask2/AddFragment.kt
@@ -9,8 +9,6 @@ import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -20,10 +18,11 @@ import java.util.*
 
 @Suppress("DEPRECATION")
 class AddFragment : Fragment() {
-    private val PICK_IMAGE_REQUEST = 1
+    private val pick_image_request = 1
     private lateinit var imageView: ImageView
     private var imageUrl: String? = null
     private lateinit var binding: FragmentAddBinding
+    private var isAllFieldsChecked: Boolean = false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
@@ -34,64 +33,79 @@ class AddFragment : Fragment() {
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.back
-        val button: Button = binding.buttonAddFragment
-        val title: EditText = binding.firstEditTextAddFragment
-        val description: EditText = binding.secondEditTextAddFragment
-        val date: EditText = binding.thirdEditTextAddFragmentDatePicker
         binding.thirdEditTextAddFragmentDatePicker.setOnClickListener {
             showDatePickerDialog()
         }
         imageView = binding.imageViewAddFragment
         pickImageFromGallery()
-        val bundle = Bundle()
         if (arguments != null && arguments?.getSerializable("searchText") != null) {
             val text = arguments?.getString("searchText")
-            title.setText(text)
-            button.text = "Түзөтүү"
+            binding.firstEditTextAddFragment.setText(text)
+            binding.buttonAddFragment.text = "Түзөтүү"
         }
         if (arguments != null && arguments?.getSerializable("editNote") != null) {
-            button.text = "Edit"
+            binding.buttonAddFragment.text = "Edit"
             val note = arguments?.getSerializable("editNote") as Note
-            title.setText(note.title)
-            description.setText(note.description)
-            date.setText(note.date)
+            binding.firstEditTextAddFragment.setText(note.title)
+            binding.secondEditTextAddFragment.setText(note.description)
+            binding.thirdEditTextAddFragmentDatePicker.setText(note.date)
             binding.buttonAddFragment.setOnClickListener {
-                val titleString = title.text.toString()
-                val descriptionString = description.text.toString()
-                val dateString = date.text.toString()
+                val titleString = binding.firstEditTextAddFragment.text.toString()
+                val descriptionString = binding.secondEditTextAddFragment.text.toString()
+                val dateString = binding.thirdEditTextAddFragmentDatePicker.text.toString()
                 val edit = Note(
                     photoResource = imageUrl,
                     title = titleString,
                     description = descriptionString,
                     date = dateString
                 )
-                (requireActivity() as MainActivity).list.add(edit)
-                val pos = arguments?.getInt("position")
-                if (pos != null) {
-                    bundle.putInt("pos", pos)
-                    requireActivity().supportFragmentManager.setFragmentResult("editNote", bundle)
-                    requireActivity().supportFragmentManager.popBackStack()
+                isAllFieldsChecked = checkAllFields()
+                if (isAllFieldsChecked) {
+                    App.db.getDao().addNote(edit)
+                    findNavController().navigateUp()
                 }
             }
         } else {
             binding.buttonAddFragment.setOnClickListener {
-                val titleString = title.text.toString()
-                val descriptionString = description.text.toString()
-                val dateString = date.text.toString()
+                val titleString = binding.firstEditTextAddFragment.text.toString()
+                val descriptionString = binding.secondEditTextAddFragment.text.toString()
+                val dateString = binding.thirdEditTextAddFragmentDatePicker.text.toString()
                 val edit = Note(
                     photoResource = imageUrl,
                     title = titleString,
                     description = descriptionString,
                     date = dateString
                 )
-                App.db.getDao().addNote(edit)
-                findNavController().navigateUp()
+                isAllFieldsChecked = checkAllFields()
+                if (isAllFieldsChecked) {
+                    App.db.getDao().addNote(edit)
+                    findNavController().navigateUp()
+                }
             }
         }
         binding.back.setOnClickListener {
             findNavController().navigateUp()
         }
+    }
+
+    private fun checkAllFields(): Boolean {
+        if (binding.firstEditTextAddFragment.length() == 0) {
+            binding.firstEditTextAddFragment.error = "THIS FIELD IS REQUIRED"
+            return false
+        }
+        if (binding.secondEditTextAddFragment.length() == 0) {
+            binding.secondEditTextAddFragment.error = "THIS FIELD IS REQUIRED"
+            return false
+        }
+        if (binding.thirdEditTextAddFragmentDatePicker.length() == 0) {
+            binding.thirdEditTextAddFragmentDatePicker.error = "THIS FIELD IS REQUIRED"
+            return false
+        }
+        if (binding.imageViewAddFragment.drawable == null) {
+            binding.error.visibility = View.VISIBLE
+            return false
+        }
+        return true
     }
 
     private fun showDatePickerDialog() {
@@ -114,7 +128,7 @@ class AddFragment : Fragment() {
         binding.imageViewAddFragment.setOnClickListener {
             val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             intent.type = "image/*"
-            startActivityForResult(intent, PICK_IMAGE_REQUEST)
+            startActivityForResult(intent, pick_image_request)
         }
     }
 
@@ -123,10 +137,11 @@ class AddFragment : Fragment() {
         super.onActivityResult(requestCode, resultCode, data)
         imageUrl = data?.data.toString()
         imageView.setImageURI(data?.data)
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data?.data != null) {
+        if (requestCode == pick_image_request && resultCode == RESULT_OK && data?.data != null) {
             try {
-                val bitmap =
-                    MediaStore.Images.Media.getBitmap(requireActivity().contentResolver, data.data)
+                val bitmap = MediaStore.Images.Media.getBitmap(
+                    requireActivity().contentResolver, data.data
+                )
                 imageView.setImageBitmap(bitmap)
             } catch (e: IOException) {
                 e.printStackTrace()
